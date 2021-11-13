5d951dbd17c6d1eefe63b68d6c86be6621721878
Elena <gingiberii@gmail.com>
2021-11-13 12:11:27 +0300

    lesson7

--- Les7/src/main/java/Main.java
+++ Les7/src/main/java/Main.java
@@ -1,5 +1,3 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.tools.sjavac.server.RequestHandler;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -8,19 +6,20 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(RequestHandler.detectCity("Moscow"));
+        String cityCode = RequestHandler.detectCity("Moscow");
+        String forecast = RequestHandler.getForecast(cityCode);
+        System.out.println(forecast);
 
 
-
-
-
-        OkHttpClient okHttpClient = new OkHttpClient();
+       /* OkHttpClient okHttpClient = new OkHttpClient();
 
         Request request = new Request.Builder()
                 .url("https://gb.ru")
                 .build();
 
         Response response = okHttpClient.newCall(request).execute();
+
+        System.out.println(response);*/
 
     }
 }
--- Les7/src/main/java/RequestHandler.java
+++ Les7/src/main/java/RequestHandler.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -7,6 +8,7 @@ import java.io.IOException;
 
 public class RequestHandler {
     final static OkHttpClient okHttpClient = new OkHttpClient();
+    final static ObjectMapper objectMapper = new ObjectMapper();
 
     public static String detectCity(String cityName) throws IOException {
 
@@ -28,6 +30,42 @@ public class RequestHandler {
                 .build();
         Response response = okHttpClient.newCall(request).execute();
         String json = response.body().string();
+
+        String city  = objectMapper.readTree(json).get(0).at("/LocalizedName").asText();
+        System.out.println(city);
+        String code = objectMapper.readTree(json).get(0).at("/Key").asText();
+
+
+        return code;
+
+
+    }
+    public static String getForecast(String cityCode) throws IOException {
+
+
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("http")
+                .host("dataservice.accuweather.com")
+                .addPathSegment("forecasts")
+                .addPathSegment("v1")
+                .addPathSegment("daily")
+                .addPathSegment("1day")
+                .addPathSegment(cityCode)
+                .addQueryParameter("apikey", "sKLqeKIYPzoYH9Nj1024Pt0hRfMbAXlZ")
+                .addQueryParameter("metric", "true")
+                .build();
+
+        Request request = new Request.Builder()
+                .addHeader("Accept", "application/json")
+                .url(httpUrl)
+                .build();
+        Response response = okHttpClient.newCall(request).execute();
+        String json = response.body().string();
+
+      /*  String city  = objectMapper.readTree(json).get(0).at("/LocalizedName").asText();
+        System.out.println(city);
+        String code = objectMapper.readTree(json).get(0).at("/Key").asText();*/
+
 
         return json;
 
